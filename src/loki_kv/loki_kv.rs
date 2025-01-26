911f94fc89412f4193056c40709997870dd7adc6
Akshat Jaimini <destrex271@gmail.com>
2025-01-27 01:16:09 +0530

    Added btreemap type; Need to update executor file

--- src/loki_kv/loki_kv.rs
+++ src/loki_kv/loki_kv.rs
@@ -1,6 +1,6 @@
 use core::{f32, panic};
 use std::any::{Any, TypeId};
-use std::collections::{self, HashMap};
+use std::collections::{self, BTreeMap, HashMap};
 use std::fmt::Debug;
 use std::mem;
 use std::ptr::null;
@@ -14,6 +14,82 @@ pub enum ValueObject {
     DecimalData(f64),
     OutputString(String),
     BlobData(Vec<u8>),
+}
+
+// Table structure with btree as internal store
+pub struct CollectionBTree{
+    store: BTreeMap<String, ValueObject>
+}
+
+impl CollectionBTree{
+    fn new() -> Self{
+        let store: BTreeMap<String, ValueObject> = BTreeMap::new();
+        CollectionBTree{
+            store
+        }
+    }
+
+    pub fn put(&mut self, key: String, value: ValueObject) -> bool {
+        let stat = self.store.insert(key, value);
+        match stat {
+            Some(stat) => {
+                println!("{:?}", stat);
+                true
+            }
+            None => false,
+        }
+    }
+
+    // Gets data
+    pub fn get(&self, key: String) -> Result<&ValueObject, String> {
+        let data = self.store.get(&key);
+
+        match data {
+            Some(dt) => Ok(dt),
+            None => Err("no data found!".to_string()),
+        }
+    }
+
+    pub fn incr(&mut self, key: String) -> Result<(), &str> {
+        let val = self.store.get(&key).unwrap();
+
+        match val {
+            ValueObject::IntData(data) => {
+                self.store.insert(key, ValueObject::IntData(data + 1));
+                Ok(())
+            }
+            ValueObject::DecimalData(data) => {
+                self.store.insert(key, ValueObject::DecimalData(data + 1.0));
+                Ok(())
+            }
+            _ => Err("incr not supported for data type"),
+        }
+    }
+
+    pub fn decr(&mut self, key: String) -> Result<(), &str> {
+        let val = self.store.get(&key).unwrap();
+
+        match val {
+            ValueObject::IntData(data) => {
+                self.store.insert(key, ValueObject::IntData(data - 1));
+                Ok(())
+            }
+            ValueObject::DecimalData(data) => {
+                self.store.insert(key, ValueObject::DecimalData(data - 1.0));
+                Ok(())
+            }
+            _ => Err("incr not supported for data type"),
+        }
+    }
+
+    // Displays all keys and values
+    pub fn display_collection(&self) -> String {
+        let mut data = String::new();
+        for (key, val) in self.store.iter() {
+            data += &format!("{:?} -> {:?}", key, val);
+        }
+        return data;
+    }
 }
 
 
@@ -93,26 +169,41 @@ impl Collection{
 }
 
 pub struct LokiKV {
-    collections: HashMap<String, Collection>,
+    collections_hmap: HashMap<String, Collection>,
+    collections_bmap: HashMap<String, Collection>,
     current_collection: String,
 }
 
 impl LokiKV {
     pub fn new() -> Self {
-        let mut collections: HashMap<String, Collection> = HashMap::new();
-        collections.insert("default".to_string(), Collection::new());
+        let mut collections_hmap: HashMap<String, Collection> = HashMap::new();
+        let mut collections_bmap: HashMap<String, Collection> = HashMap::new();
+        collections_hmap.insert("default".to_string(), Collection::new());
         LokiKV {
-            collections,
+            collections_hmap,
+            collections_bmap,
             current_collection: "default".to_string()
         }
     }
 
-    pub fn create_collection(&mut self, collection_name: String){
-        self.collections.insert(collection_name, Collection::new());
+    pub fn create_hmap_collection(&mut self, collection_name: String){
+        self.collections_hmap.insert(collection_name, Collection::new());
     }
 
-    pub fn select_collection(&mut self, key: String){
-        if self.collections.contains_key(&key){
+    pub fn create_bmap_collection(&mut self, collection_name: String){
+        self.collections_bmap.insert(collection_name, Collection::new());
+    }
+
+    pub fn select_hmap_collection(&mut self, key: String){
+        if self.collections_hmap.contains_key(&key){
+        self.current_collection = key;
+        }else{
+            panic!("Collection not found!")
+        }
+    }
+
+    pub fn select_bmap_collection(&mut self, key: String){
+        if self.collections_bmap.contains_key(&key){
         self.current_collection = key;
         }else{
             panic!("Collection not found!")
@@ -124,11 +215,31 @@ impl LokiKV {
     }
 
     pub fn get_current_collection_mut(&mut self) -> &mut Collection{
-        self.collections.get_mut(&self.current_collection).expect("Current collection missing..")
+        match self.collections_hmap.get_mut(&self.current_collection){
+            Some(x) => {
+                x
+            },
+            None => {
+                match self.collections_bmap.get_mut(&self.current_collection){
+                    Some(x) => x,
+                    None => panic!("Not found!")
+                }
+            }
+        }
     }
 
     pub fn get_current_collection(&self) -> &Collection{
-        self.collections.get(&self.current_collection).expect("Current collection missing..")
+        match self.collections_hmap.get(&self.current_collection){
+            Some(x) => {
+                x
+            },
+            None => {
+                match self.collections_bmap.get(&self.current_collection){
+                    Some(x) => x,
+                    None => panic!("Not found!")
+                }
+            }
+        }
     }
 
     // Inserts Data
@@ -156,7 +267,11 @@ impl LokiKV {
 
     pub fn get_all_collection_names(&self) -> String{
         let mut res: String = String::new();
-        for (key, _) in self.collections.iter(){
+        for (key, _) in self.collections_hmap.iter(){
+            res += &key.clone();
+            res += "\n";
+        }
+        for (key, _) in self.collections_bmap.iter(){
             res += &key.clone();
             res += "\n";
         }
