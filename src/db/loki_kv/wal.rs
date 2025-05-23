555119e7b2a56538b7f98c24870a9c2cef7df05d
Akshat Jaimini <destrex271@gmail.com>
2025-05-23 12:19:38 +0000

    wal

--- src/db/loki_kv/wal.rs
+++ src/db/loki_kv/wal.rs
@@ -2,11 +2,20 @@ use std::path::Path;
 use std::io::Write;
 use std::fs::File;
 use std::time::SystemTime;
+use std::fs::OpenOptions;
 
 pub struct WALManager{
     filename: String,
     directory: String,
-    full_path: Box<Path>
+    full_path: Path,
+    buffer: HashSet<usize>,
+    DUMP_THRESHOLD: usize
+}
+
+struct WALObject{
+    collection_name: String,
+    key: String,
+    value: String,
 }
 
 fn get_cur_timestamp_as_str() -> String {
@@ -16,8 +25,10 @@ fn get_cur_timestamp_as_str() -> String {
     }
 }
 
+fn dump_hash_set(buffer: HashSet<WALObject>)
+
 impl WALManager{
-    fn new(instance_id: String, directory: String) -> WALManager{
+    fn new(&self, instance_id: String, directory: String, dump_threshold: usize) -> WALManager{
         let filename = format!("{}_{}.wal",instance_id, get_cur_timestamp_as_str());
 
         let full_path = match directory.len(){
@@ -25,43 +36,38 @@ impl WALManager{
             _ => Path::new(format!("{}/{}", directory, filename))
         };
 
+        let hashset: HashSet<WALObject> = HashSet::new();
         WALManager{
             filename,
             directory,
-            full_path
+            full_path,
+            buffer: hashset,
+            DUMP_THRESHOLD: dump_threshold
         }
     }
-    fn append_to_log(&self, command: String) -> bool{
-        let mut file: File = match File::options().append(true).open(self.full_path) {
-            Ok(file) => file,
-            Err(_) => panic!("failed to create"),
-        };
-        
-        match writeln!(&mut file, "{}", command){
-            Ok(a) => {
-                println!("{:?}", a);
-                return true;
-            }
-            Err(err) => false 
+    fn append_to_log(&mut self, colname: String, key: String, value: String, typ: String) -> bool{
+        self.hashset.insert(format!("[colname: {:?}\tkey: {:?}\tvalue: {:?}\ttype: {:?}]", colname, key, value, typ));    
+        if hashset.len() > self.DUMP_THRESHOLD{
+            dump_hash_set(self.hashset.clone());
+            self.hashset.clear();
         }
     }
-    fn read_log(&self) -> bool{
-        false
-    }
+
+    fn read_log(&self) -> bool{}
 }
 
-#[cfg(test)]
+
 mod tests {
-    use crate::loki_kv::wal::WALManager;
     use super::*;
 
     #[test]
-    fn test_wal_write() {
-        let file_content = vec!["SET data 12", "SET data1 <BLOB_BEGINS>blob data<BLOB_ENDS>"];
-        let wal_mgr = WALManager::new(String::from("12"), String::from(""));
-
-        for item in file_content{
-            wal_mgr.append_to_log(String::from(item));
-        }
+    fn try_wal() {
+        let mut mgr = WALManager::new("dummy-wal", "");
+        mgr.append_to_log("data", "12", "Int");
+        mgr.append_to_log("data", "12", "Int");
+        mgr.append_to_log("data", "12", "Int");
+        mgr.append_to_log("data", "12", "Int");
+        // validate file?
     }
+
 }
