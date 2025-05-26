a2bbc139f68caa4dd6702cd770b6de66c5f7bf28
Akshat Jaimini <destrex271@gmail.com>
2025-05-26 10:47:44 +0000

    removed some errors

--- src/db/loki_kv/wal.rs
+++ src/db/loki_kv/wal.rs
@@ -1,17 +1,19 @@
-use std::path::Path;
+use std::path::{PathBuf};
 use std::io::Write;
 use std::fs::File;
 use std::time::SystemTime;
 use std::fs::OpenOptions;
+use std::collections::HashSet;
 
 pub struct WALManager{
     filename: String,
     directory: String,
-    full_path: Path,
-    buffer: HashSet<usize>,
+    full_path: PathBuf,
+    buffer: HashSet<WALObject>,
     DUMP_THRESHOLD: usize
 }
 
+#[derive(Eq, Hash, PartialEq,Clone)]
 struct WALObject{
     collection_name: String,
     key: String,
@@ -20,40 +22,49 @@ struct WALObject{
 
 fn get_cur_timestamp_as_str() -> String {
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => format!("1970-01-01 00:00:00 UTC"),
+        Ok(n) => format!("{:?}", n),
         Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     }
 }
 
-fn dump_hash_set(buffer: HashSet<WALObject>)
+fn dump_hash_set(buffer: HashSet<WALObject>, collection_name: String) -> Result<String, String>{
+    Ok(String::from("hello"))
+}
 
 impl WALManager{
     fn new(&self, instance_id: String, directory: String, dump_threshold: usize) -> WALManager{
         let filename = format!("{}_{}.wal",instance_id, get_cur_timestamp_as_str());
 
         let full_path = match directory.len(){
-            0 => Path::new(format!("./{}", filename)),
-            _ => Path::new(format!("{}/{}", directory, filename))
+            0 => Path::new(format!("./{}", filename).as_str()),
+            _ => Path::new(format!("{}/{}", directory, filename).as_str())
         };
 
         let hashset: HashSet<WALObject> = HashSet::new();
         WALManager{
             filename,
             directory,
-            full_path,
+            full_path: full_path.to_owned(),
             buffer: hashset,
             DUMP_THRESHOLD: dump_threshold
         }
     }
     fn append_to_log(&mut self, colname: String, key: String, value: String, typ: String) -> bool{
-        self.hashset.insert(format!("[colname: {:?}\tkey: {:?}\tvalue: {:?}\ttype: {:?}]", colname, key, value, typ));    
-        if hashset.len() > self.DUMP_THRESHOLD{
-            dump_hash_set(self.hashset.clone());
-            self.hashset.clear();
+        self.buffer.insert(
+
+        );
+        self.buffer.insert(format!("[colname: {:?}\tkey: {:?}\tvalue: {:?}\ttype: {:?}]", colname, key, value, typ));    
+        if self.buffer.len() > self.DUMP_THRESHOLD{
+            dump_hash_set(self.buffer.clone(), colname);
+            self.buffer.clear();
         }
+
+        return true;
     }
 
-    fn read_log(&self) -> bool{}
+    fn read_log(&self) -> bool{
+        true
+    }
 }
 
 
