016948de2dd13540ebf35483f42b6a7c73de43c5
destrex271 <destrex271@gmail.com>
2025-09-29 08:20:51 +0530

    base wal

--- src/db/loki_kv/control.rs
+++ src/db/loki_kv/control.rs
@@ -1,18 +1,28 @@
-use std::{fs::File, io::Write, path::Path};
 use bincode;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    path::Path,
+};
 
-#[derive(Serialize)]
-pub struct ControlFile{
+#[derive(Serialize, Deserialize)]
+pub struct ControlFile {
     last_wal_timeline: u64,
     last_checkpoint_id: u64,
     checkpoint_directory_path: String,
     wal_directory_path: String,
 }
 
-impl ControlFile{
-    fn write(path: String, last_wal_timeline: u64, last_checkpoint_id: u64, checkpoint_directory_path: String, wal_directory_path: String) -> Result<String, String>{
-        let ctrl_file = ControlFile{
+impl ControlFile {
+    pub fn write(
+        path: String,
+        last_wal_timeline: u64,
+        last_checkpoint_id: u64,
+        checkpoint_directory_path: String,
+        wal_directory_path: String,
+    ) -> Result<String, String> {
+        let ctrl_file = ControlFile {
             last_wal_timeline,
             last_checkpoint_id,
             checkpoint_directory_path,
@@ -22,21 +32,39 @@ impl ControlFile{
         // Take lock on control file
         let path = Path::new(path.as_str());
         // Write to control file
-        match File::open(&path){
+        match File::open(&path) {
             Ok(mut file) => {
                 // take a lock on the file
-                match File::try_lock(&file){
-                    Ok(_)=>{
+                match File::try_lock(&file) {
+                    Ok(_) => {
                         let vc: Vec<u8> = bincode::serialize(&ctrl_file).unwrap();
                         file.write_all(&vc);
                         Ok(format!("written to control file : {}", path.display()))
-                    },
-                    Err(err) => {
-                        Err(err.to_string())
                     }
+                    Err(err) => Err(err.to_string()),
                 }
-            },
-            Err(err) => return Err(err.to_string())
+            }
+            Err(err) => return Err(err.to_string()),
+        }
+    }
+
+    pub fn read_from_file_path(path_string: String) -> Result<ControlFile, String> {
+        let path = Path::new(path_string.as_str());
+        match File::open(&path) {
+            Ok(mut file) => {
+                // take a lock on the file
+                match File::try_lock(&file) {
+                    Ok(_) => {
+                        let mut buffer = Vec::new();
+                        file.read_to_end(&mut buffer).unwrap();
+                        let ctrl_file: ControlFile = bincode::deserialize(&buffer).unwrap();
+                        println!("Read control file: {:?}", path_string);
+                        Ok(ctrl_file)
+                    }
+                    Err(err) => return Err(format!("Failed to lock file: {}", err)),
+                }
+            }
+            Err(err) => Err(format!("Failed to open file: {}", err)),
         }
     }
 }
--- src/db/loki_kv/wal.rs
+++ src/db/loki_kv/wal.rs
@@ -1,29 +1,45 @@
-use std::path::{PathBuf, Path};
-use std::io::Write;
-use std::fs::{File, OpenOptions};
-use std::time::SystemTime;
 use std::collections::HashSet;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
 
+use crate::loki_kv::control::ControlFile;
 use crate::loki_kv::loki_kv::ValueObject;
 
-pub struct WALRecord{
-    timeline_id: u64,
+pub struct WALRecord {
+    timestamp: u64,
     key: String,
     value: ValueObject,
 }
 
-impl WALRecord{
-    fn new(timeline_id: u64, key: String, value: ValueObject) -> Self{
-        WALRecord { timeline_id: timeline_id, key: key, value: value}
+impl WALRecord {
+    fn new(timeline_id: u32, key: String, value: ValueObject) -> Self {
+        WALRecord {
+            timestamp: timeline_id,
+            key: key,
+            value: value,
+        }
     }
 }
 
-
 // ----------- WAL Record Manager ---------------------
 // Responsible for routing WAL records to timeline buffer
-// Once a timeline is flushed, the timeline reference is 
+// Once a timeline is flushed, the timeline reference is
 // kept for future reference.
-// pub struct WALManager{
-//     control_file: ControlFile,
-// }
+pub struct WALManager {
+    control_file: ControlFile,
+    wal_records: HashSet<WALRecord>,
+    cur_timeline: u32,
+}
 
+impl WALManager {
+    fn new(ctrl_file_path: String) -> Self {
+        let control_file: ControlFile = ControlFile::read_from_file_path(ctrl_file_path).unwrap();
+        WALManager {
+            control_file,
+            wal_records: HashSet::new(),
+            cur_timeline: 0,
+        }
+    }
+}
